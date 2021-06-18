9f994cd6eb99f3b1f4206a119454293dac0d617a
wajeht <58354193+wajeht@users.noreply.github.com>
2021-06-18 01:25:08 -0500

    bug: fix paths for diff env

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const AmericanQuarterHorseAssociationMembership = require("./src/AmericanQuarterHorseAssociationMembership.js");
-const config = require("./config.json");
+const AmericanQuarterHorseAssociationMembership = require('./src/AmericanQuarterHorseAssociationMembership.js');
+const config = require('./config.json');
 
 (function main() {
     const myTask = new AmericanQuarterHorseAssociationMembership();
-    myTask.DEBUG = true;
+    myTask.DEBUG = true; // this will turn on verboose mode
 
+    // Production
+    // const outputFiles = myTask.getFiles(config.windows.OMEGASRV1_ADF_OUTPUT, config.windows.FILE_EXTENSION);
+    // myTask.copyFiles(outputFiles, config.windows.SPDE_SHARE);
+
+    // const filesToRename = myTask.getFiles(config.windows.SPDE_SHARE, config.windows.FILE_EXTENSION);
+    // myTask.renameFiles(filesToRename, config.windows.TO_RENAME);
+
+    // Dev
     const outputFiles = myTask.getFiles(
-        config.OMEGASRV1_ADF_OUTPUT,
-        config.FILE_EXTENSION
+        config.development.mac.OMEGASRV1_ADF_OUTPUT,
+        config.development.mac.FILE_EXTENSION
     );
-
-    myTask.copyFiles(outputFiles, config.SPDE_SHARE);
+    myTask.copyFiles(outputFiles, config.development.mac.SPDE_SHARE);
 
     const filesToRename = myTask.getFiles(
-        config.SPDE_SHARE,
-        config.FILE_EXTENSION
+        config.development.mac.SPDE_SHARE,
+        config.development.mac.FILE_EXTENSION
     );
-
-    myTask.renameFiles(filesToRename, config.TO_RENAME);
+    myTask.renameFiles(filesToRename, config.development.mac.TO_RENAME);
 })();
--- src/AmericanQuarterHorseAssociationMembership.js
+++ src/AmericanQuarterHorseAssociationMembership.js
@@ -1,186 +1,178 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 class AmericanQuarterHorseAssociationMembership {
-  constructor() {
-    this.DEBUG = false;
-  }
-
-  /**
-   * Turn on debug mode with verboose output
-   * @param {String} message
-   */
-  #debug(message) {
-    if (this.DEBUG && message) {
-      console.debug(message);
+    constructor() {
+        this.DEBUG = false;
     }
-  }
 
-  /**
-   * Figure out how old the file is.
-   * @param {String} filePath
-   * @returns {String}
-   */
-  #getFileDate(filePath) {
-    const date = new Date(fs.statSync(filePath).mtime)
-      .toUTCString()
-      .split(" ")
-      .slice(0, 4)
-      .join(" ");
-    return date;
-  }
-
-  /**
-   * Get files will return files with extension that user set which are created today
-   * @param {String} folderPath
-   * @param {String} fileExtension
-   * @returns {Array}
-   */
-  getFiles(folderPath, fileExtension) {
-    try {
-      this.#debug();
-      this.#debug({
-        function: ` ########## renameFiles(${folderPath}, ${fileExtension})  ########## `,
-      });
-
-      const tempFileArray = [];
-
-      // check to see for valid path
-      if (fs.statSync(folderPath).isDirectory()) {
-        const filesInsideCurrentFolder = fs.readdirSync(folderPath);
-
-        for (let currentFile of filesInsideCurrentFolder) {
-          const currentFileAbsolutePath = path.join(folderPath, currentFile);
-
-          const currentFileExtension = currentFileAbsolutePath.split(".").pop();
-          const currentFileDate = this.#getFileDate(currentFileAbsolutePath);
-          const todaysDate = new Date()
-            .toUTCString()
-            .split(" ")
-            .slice(0, 4)
-            .join(" ");
-
-          if (
-            currentFileExtension === fileExtension &&
-            todaysDate === currentFileDate
-          ) {
-            this.#debug({
-              fileFound: currentFileAbsolutePath,
-              currentFileDate: currentFileDate,
-            });
-
-            tempFileArray.push({
-              currentFile: currentFileAbsolutePath,
-              fileDate: this.#getFileDate(currentFileAbsolutePath),
-            });
-          }
+    /**
+     * Turn on debug mode with verboose output
+     * @param {String} message
+     */
+    #debug(message) {
+        if (this.DEBUG && message) {
+            console.debug(message);
         }
-
-        return tempFileArray;
-      }
-
-      throw new Error("bad folder path");
-    } catch (error) {
-      console.log(error.message);
     }
-  }
 
-  /**
-   * Copy all the files we keep in track, into another destination
-   * @param {Array} fileArray
-   * @param {String} renameText
-   */
-  copyFiles(fileArray, destinationPath) {
-    this.#debug();
-    this.#debug({
-      function: ` ########## copyFiles(${
-        typeof fileArray === ""
-          ? fileArray
-          : fileArray[0].currentFile
-              .split("\\")
-              .slice(0, fileArray[0].currentFile.split("\\").length - 1)
-              .join("\\")
-              .concat(`${fileArray[0].currentFile.split(".")}`)
-      }, ${destinationPath})  ########## `,
-    });
+    /**
+     * Figure out how old the file is.
+     * @param {String} filePath
+     * @returns {String}
+     */
+    #getFileDate(filePath) {
+        const date = new Date(fs.statSync(filePath).mtime)
+            .toUTCString()
+            .split(' ')
+            .slice(0, 4)
+            .join(' ');
+        return date;
+    }
 
-    try {
-      for (let file of fileArray) {
-        const currentFile = file.currentFile;
-        const fileName = currentFile.split("\\").pop();
+    /**
+     * Get files will return files with extension that user set which are created today
+     * @param {String} folderPath
+     * @param {String} fileExtension
+     * @returns {Array}
+     */
+    getFiles(folderPath, fileExtension) {
+        try {
+            this.#debug();
+            this.#debug({
+                function: ` ########## getFiles(${folderPath}, ${fileExtension})  ########## `,
+            });
 
-        // I was passing in just folde path, and it did not work
-        // I have to put path with file name on destination
-        // That's what below line do!
-        const destinationPathWithFileName = `${destinationPath}${fileName}`;
+            const tempFileArray = [];
 
-        // Async code, this was messing up debug output so i had to use sync code
-        // because this return after the whatever function called below this
-        // fs.copyFile(currentFile, destinationPathWithFileName, (error) => {
-        //   if (error) {
-        //     throw new Error("file copying went wrong!");
-        //   }
+            // check to see for valid path
+            if (fs.statSync(folderPath).isDirectory()) {
+                const filesInsideCurrentFolder = fs.readdirSync(folderPath);
 
-        //   console.debug({
-        //     fileCopied: fileName,
-        //     to: destinationPathWithFileName,
-        //   });
+                for (let currentFile of filesInsideCurrentFolder) {
+                    const currentFileAbsolutePath = path.join(folderPath, currentFile);
 
-        // });
+                    const currentFileExtension = currentFileAbsolutePath.split('.').pop();
+                    const currentFileDate = this.#getFileDate(currentFileAbsolutePath);
+                    const todaysDate = new Date().toUTCString().split(' ').slice(0, 4).join(' ');
 
-        fs.copyFileSync(currentFile, destinationPathWithFileName);
+                    if (currentFileExtension === fileExtension && todaysDate === currentFileDate) {
+                        this.#debug({
+                            fileFound: currentFileAbsolutePath,
+                            currentFileDate: currentFileDate,
+                        });
+
+                        tempFileArray.push({
+                            currentFile: currentFileAbsolutePath,
+                            fileDate: this.#getFileDate(currentFileAbsolutePath),
+                        });
+                    }
+                }
+
+                return tempFileArray;
+            }
+
+            throw new Error('bad folder path');
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    /**
+     * Copy all the files from array of object.currentFile into another destination path
+     * @param {Array} fileArray                 // For Example:     [{currentFile: '/Users/Download/'}, {}]
+     * @param {String} Destination Path         // For Example:     '/Users/Download/'
+     */
+    copyFiles(fileArray, destinationPath) {
+        this.#debug();
         this.#debug({
-          fileCopied: currentFile,
-          to: destinationPath,
+            function: ` ########## copyFiles(${
+                typeof fileArray === ''
+                    ? fileArray
+                    : fileArray[0].currentFile
+                          .split('\\')
+                          .slice(0, fileArray[0].currentFile.split('\\').length - 1)
+                          .join('\\')
+                          .concat(`${fileArray[0].currentFile.split('.')}`)
+            }, ${destinationPath})  ########## `,
         });
-      }
-    } catch (error) {
-      console.log(error.message);
+
+        try {
+            for (let file of fileArray) {
+                const currentFile = file.currentFile;
+                // const fileName = currentFile.split('\\').pop(); // windows
+                const fileName = currentFile.split('/').pop(); // unix
+
+                // I was passing in just folde path, and it did not work
+                // I have to put path with file name on destination
+                // That's what below line do!
+                const destinationPathWithFileName = `${destinationPath}${fileName}`;
+
+                // Async code, this was messing up debug output so i had to use sync code
+                // because this return after the whatever function called below this
+                // fs.copyFile(currentFile, destinationPathWithFileName, (error) => {
+                //   if (error) {
+                //     throw new Error("file copying went wrong!");
+                //   }
+
+                //   console.debug({
+                //     fileCopied: fileName,
+                //     to: destinationPathWithFileName,
+                //   });
+                // });
+
+                fs.copyFileSync(currentFile, destinationPathWithFileName);
+                this.#debug({
+                    fileCopied: currentFile,
+                    to: destinationPath,
+                });
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
     }
-  }
 
-  /**
-   * Rename all the files from an input array to the whatever we want
-   * @param {Array} fileArray
-   * @param {String} renameText
-   */
-  renameFiles(fileArray, renameText) {
-    try {
-      this.#debug();
-      this.#debug({
-        function: ` ########## copyFiles(${
-          typeof fileArray === ""
-            ? fileArray
-            : fileArray[0].currentFile
-                .split("\\")
-                .slice(0, fileArray[0].currentFile.split("\\").length - 1)
-                .join("\\")
-                .concat(`${fileArray[0].currentFile.split(".")}`)
-        }, ${renameText})  ########## `,
-    });
-  
-      for (let file of fileArray) {
-        const currentFile = file.currentFile;
-        const currentFileWithoutFileExtension = file.currentFile
-          .split(".")
-          .shift();
-        const fileExtension = `.${file.currentFile.split(".").pop()}`; // get extention from file path
-        const toRenamed = `${currentFileWithoutFileExtension}_${renameText}${fileExtension}`; // put the extension back, thus become pull path with extension
+    /**
+     * Rename all the files from an input array to the whatever we want
+     * @param {Array} fileArray
+     * @param {String} renameText
+     */
+    renameFiles(fileArray, renameText) {
+        try {
+            this.#debug();
+            this.#debug({
+                function: ` ########## renameFiles(${
+                    typeof fileArray === ''
+                        ? fileArray
+                        : fileArray[0].currentFile
+                              .split('\\')
+                              .slice(0, fileArray[0].currentFile.split('\\').length - 1)
+                              .join('\\')
+                              .concat(`${fileArray[0].currentFile.split('.')}`)
+                }, ${renameText})  ########## `,
+            });
 
-        fs.rename(currentFile, toRenamed, (error) => {
-          if (error) {
-            throw new Error("file renaming went wrong!");
-          }
+            for (let file of fileArray) {
+                const currentFile = file.currentFile;
+                const currentFileWithoutFileExtension = file.currentFile.split('.').shift();
+                const fileExtension = `.${file.currentFile.split('.').pop()}`; // get extention from file path
+                const toRenamed = `${currentFileWithoutFileExtension}_${renameText}${fileExtension}`; // put the extension back, thus become pull path with extension
 
-          this.#debug({
-            currentFile: currentFile,
-            renamedTo: toRenamed,
-          });
-        });
-      }
-    } catch (error) {
-      console.log(error.message);
+                fs.rename(currentFile, toRenamed, (error) => {
+                    if (error) {
+                        throw new Error('file renaming went wrong!');
+                    }
+
+                    this.#debug({
+                        currentFile: currentFile,
+                        renamedTo: toRenamed,
+                    });
+                });
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
     }
-  }
+}
 
 module.exports = AmericanQuarterHorseAssociationMembership;
